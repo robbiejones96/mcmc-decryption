320a0e4697d330ceb2974159a7bf9f6727afbb27
robbiejones96 <robbiejones96@gmail.com>
2019-07-27 01:35:37 -0700

    First attempt with bigram probabilities

--- mcmc-test.py
+++ mcmc-test.py
@@ -6,15 +6,16 @@ class TestHelperMethods(unittest.TestCase):
 	"""
 		Tests that all possible letter bigrams are present in the map
 	"""
-	def test_all_bigrams_present(self):
+	def test_bigram_probs(self):
 		counts_file = mcmc.BIGRAM_COUNTS
-		log_probs = mcmc.read_log_probs(counts_file)
+		probs = mcmc.read_probs(counts_file)
+		self.assertTrue(abs(1.0 - sum(probs[bigram] for bigram in probs)) < 1e-7)
 		for i in range(25):
 			char_i = chr(i + ord('a'))
 			for j in range(25):
 				char_j = chr(j + ord('a'))
-				bigram = (char_i + char_j).upper()
-				self.assertTrue(bigram in log_probs)
+				bigram = (char_i + char_j)
+				self.assertTrue(bigram in probs)
 
 	"""
 		Tests text permutation with a simple mapping: 
@@ -33,14 +34,22 @@ class TestHelperMethods(unittest.TestCase):
 		permuted_text = mcmc.permute_text(text, mapping)
 		self.assertEqual(expected_text, permuted_text)
 
+		text = "Mr. Green's 2nd ad-hoc experiment!"
+		expected_text = "Ns. Hsffo't 2oe be-ipd fyqfsjnfou!"
+		permuted_text = mcmc.permute_text(text, mapping)
+		self.assertEqual(expected_text, permuted_text)
+
 	"""
-		Verifies that two (and only two) letters are correctly swapped
+		Verifies that two (or possibly 0) letters are correctly swapped
 		in the permuted map
 	"""
 	def test_map_permutation(self):
 		mapping = mcmc.get_random_mapping()
 		permuted_mapping = mcmc.get_permuted_mapping(mapping)
 		changed_chars = [char for char in mapping if mapping[char] != permuted_mapping[char]]
+		if len(changed_chars) == 0: # no letters were swapped, nothing else to check
+			return
+
 		self.assertEqual(2, len(changed_chars))
 		self.assertEqual(mapping[changed_chars[0]], permuted_mapping[changed_chars[1]])
 		self.assertEqual(mapping[changed_chars[1]], permuted_mapping[changed_chars[0]])
--- mcmc.py
+++ mcmc.py
@@ -1,33 +1,36 @@
-BIGRAM_COUNTS = "english_bigrams_1.txt"
-from math import log
-from random import shuffle, randint
+BIGRAM_COUNTS = "bigrams.json"
+from math import log, exp
+from random import shuffle, randint, random
+import json
 
-NUM_ITERATIONS = 1000
-TEMP = 0.1
+NUM_ITERATIONS = 10000
+TEMP = 10
 
-def read_log_probs(counts_file):
+def read_probs(counts_file):
 	counts = {}
 	total_count = 0
-	with open(counts_file, 'r') as file:
-		for line in file:
-			bigram, count = line.strip().split()
-			counts[bigram] = int(count)
-			total_count += int(count)
-	log_probs = { bigram : log(counts[bigram] / total_count) for bigram in counts }
-	return log_probs
+	counts_list = json.load(open(counts_file, 'r'))
+	for (bigram, count) in counts_list:
+		counts[bigram] = count
+		total_count += count
+	probs = { bigram : counts[bigram] / total_count for bigram in counts }
+	return probs
 
-def get_word_log_prob(word, log_probs):
+def get_word_log_prob(word, probs):
 	log_prob = 0
 	for i in range(len(word) - 1):
-		bigram = word[i : i + 2].upper()
-		log_prob += log_probs[bigram]
+		bigram = word[i : i + 2].lower()
+		prob = probs[bigram]
+		if prob == 0:
+			return float("-inf")
+		log_prob += log(prob)
 	return log_prob
 
-def get_text_log_prob(text, log_probs):
+def get_text_log_prob(text, probs):
 	log_prob = 0
 	for word in text.strip().split():
 		word = "".join(letter for letter in word.strip() if letter.isalpha())
-		log_prob += get_word_log_prob(word.strip(), log_probs)
+		log_prob += get_word_log_prob(word.strip(), probs)
 	return log_prob
 
 def get_random_mapping():
@@ -54,13 +57,33 @@ def permute_text(text, mapping):
 			permuted_text[i] = permuted_char
 	return "".join(permuted_text)
 
-def run_mcmc(text, num_iterations, temp):
-	pass
+def run_mcmc(text, num_iterations, temp=0):
+	probs = read_probs(BIGRAM_COUNTS)
+	curr_mapping = { chr(i + ord('a')) : chr(i + ord('a')) for i in range(26) }
+	curr_score = get_text_log_prob(text, probs)
+	best_mapping, best_score = curr_mapping, curr_score
+	for it in range(num_iterations):
+		new_mapping = get_permuted_mapping(curr_mapping)
+		new_text = permute_text(text, new_mapping)
+		new_score = get_text_log_prob(new_text, probs)
+		delta = new_score - curr_score
+		if delta > 0 or temp > 0 and random() < exp(delta / temp):
+			curr_mapping = new_mapping
+			curr_score = new_score
+			if curr_score > best_score:
+				print("Found new best score: ", curr_score)
+				best_score = curr_score
+				best_mapping = curr_mapping
+	return best_mapping 
 
 def main():
-	log_probs = read_log_probs(BIGRAM_COUNTS)
 	text = input("Provide some text: ")
-	print("Log probability of text is: ", get_text_log_prob(text, log_probs))
+	random_mapping = get_random_mapping()
+	permuted_text = permute_text(text, random_mapping)
+	print("Encrypted text is ", permuted_text)
+	best_mapping = run_mcmc(permuted_text, NUM_ITERATIONS, TEMP)
+	decrypted_text = permute_text(permuted_text, best_mapping)
+	print("Decrypted text is ", decrypted_text)
 
 if __name__ == "__main__":
 	main()
